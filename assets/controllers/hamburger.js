80435e28b73a307f276b748d2d691090c8f9b9dc
Aaron Wilches Tamara <elijahahraa@gmail.com>
2025-04-10 16:20:46 -0500

    Barra de navegacion terminada, con el menu hamburger en dispositivos moviles, falta hacerlo para pantallas grandes

--- assets/controllers/hamburger.js
+++ assets/controllers/hamburger.js
@@ -5,8 +5,14 @@ const menu = d.querySelector('.hamburger');
 
 export default function hamburgerMenu(btnHamburger, panel, menuLink) {
     menu.addEventListener("click", (e)=>{
-        console.log("Hola mundo");
-        
+        if (e.target.matches(btnHamburger) || e.target.matches(`${btnHamburger} *`)) {
+            d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(btnHamburger).classList.toggle("is-active");
+        }
+        if (e.target.matches(menuLink)) {
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(btnHamburger).classList.remove("is-active");
+        }
     })
 }
 
