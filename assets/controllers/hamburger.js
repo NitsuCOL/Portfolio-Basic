c43c9eacd20424db67fea0e42521e13e1b08f67e
Aaron Wilches Tamara <elijahahraa@gmail.com>
2025-04-11 11:26:01 -0500

    Diseño terminado, faltan validaciones del formulario

--- assets/controllers/hamburger.js
+++ assets/controllers/hamburger.js
@@ -1,19 +1,17 @@
 const d = document;
 
-const panelIsActive = d.querySelector(".list_nav_container");
-const menu = d.querySelector('.hamburger');
-
 export default function hamburgerMenu(btnHamburger, panel, menuLink) {
-    menu.addEventListener("click", (e)=>{
+    d.addEventListener("click", (e) => {
         if (e.target.matches(btnHamburger) || e.target.matches(`${btnHamburger} *`)) {
             d.querySelector(panel).classList.toggle("is-active");
             d.querySelector(btnHamburger).classList.toggle("is-active");
         }
+
         if (e.target.matches(menuLink)) {
             d.querySelector(panel).classList.remove("is-active");
             d.querySelector(btnHamburger).classList.remove("is-active");
         }
-    })
+    });
 }
 
 hamburgerMenu(".hamburger", ".panel", ".menu_link");
